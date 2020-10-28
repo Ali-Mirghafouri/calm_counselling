3e67302511abb33a1f8ce57023aede468d901059
Ali <ali.mirghafouri@gmail.com>
2020-10-28 23:47:36 +0800

    navigation and nav bar

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,34 @@
 import "./App.css"
+import logo from "./calm_counselling_logo.png"
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom"
+
+const tabOptions = ["Home", "About", "Services", "Gallery", "Contact"]
 
 function App() {
-  return <div>hello</div>
+  return (
+    <Router>
+      <Redirect to="/Home" />
+      <div className="app_nav_container">
+        <div className="app_img_container">
+          <img src={logo} alt="calm counselling logo"></img>
+        </div>
+        <div className="app_nav_options_container">
+          {tabOptions.map((tabName) => (
+            <a className="app_nav_option" href={"/" + tabName}>
+              {tabName}
+            </a>
+          ))}
+        </div>
+      </div>
+      <Switch>
+        <Route path="/about"></Route>
+        <Route path="/users"></Route>
+        <Route path="/Home"></Route>
+        <Route path="/Gallery"></Route>
+        <Route path="/Contact"></Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
